3e53a7480bbf642992e25dfc912a89de46ee2653
Ines Cunha <inescunha@student.dei.uc.pt>
2024-03-13 00:57:17 +0000

    view users

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import './assets/background-video/background-video.jsx';
 import BackgroundLoginVideo from './assets/background-video/background-video.jsx';
 import ModalLogin from './modal-login/modal-login.jsx';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from './footer/footer.jsx';'./footer/footer.jsx';
 import MenuAside from './menuAside/menuAside.jsx';
 import Column from './column/column.jsx';
@@ -17,6 +17,7 @@ function App() {
   const [userName, setUserName] = useState('');
   const [userPhoto, setUserPhoto] = useState('');
   const [isUsersView, setIsUsersView] = useState(false);
+  const [users, setUsers] = useState([]);
 
 
   const handleLogin = () => {
@@ -27,6 +28,34 @@ function App() {
     setUserName(name);
     setUserPhoto(photo);
   };
+  useEffect(() => {
+    async function fetchUsers() {
+      const token = sessionStorage.getItem('token'); 
+
+      const response = await fetch('http://localhost:8080/Scrum_Project_4_war_exploded/rest/user/all', {
+        method: 'GET',
+        headers: {
+          Accept: '*/*',
+          'Content-Type': 'application/json',
+          token: token,
+        }
+      });
+
+      if (response.status === 200) {
+        const usersArray = await response.json();
+        console.log(usersArray);
+        if (usersArray.length === 0) {
+          console.log("Users not found");
+        } else {
+          setUsers(usersArray);
+        }
+      } else if (response.status === 403) {
+        console.log("Unauthorized access");
+      }
+    }
+
+    fetchUsers();
+  }, []);
 
   return (
     <div className='App'>
@@ -47,9 +76,9 @@ function App() {
           </aside>
           {isUsersView ? (
             <div className="columns">
-              <Column title="Developer" />
-              <Column title="Scrum Master" />
-              <Column title="Product Owner" />
+             <Column title="Developer" users={users.filter(user => user.role === 'developer')} />
+             <Column title="Scrum Master" users={users.filter(user => user.role === 'ScrumMaster')} />
+             <Column title="Product Owner" users={users.filter(user => user.role === 'Owner')} />
             </div>
           ) : (
             <div className="columns">
--- src/column/column.jsx
+++ src/column/column.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './column.css';
+import UserCard from '../userCard';
 
-function Column({ title }) {
+function Column({ title, users }) {
     return (
-        <div className="column">
-            <h2>{title}</h2>
-            {}
+      <div className="column">
+        <h2>{title}</h2>
+        <div className="column-content">
+          {users && users.map(user => (
+            <UserCard key={user.username} name={user.name} photo={user.userPhoto} />
+          ))}
         </div>
+      </div>
     );
-}
+  }
 
 export default Column;
